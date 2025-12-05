2d96c7b864795ef04228ac420aaddae3bae00ad8
jordonbinns <jordonbinns@gmail.com>
2025-12-05 16:55:20 -0500

    Did exercise 4

--- world.php
+++ world.php
@@ -6,23 +6,36 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-// Read the GET variable 
+// Read GET variable
 $country = $_GET['country'] ?? "";
 
-// Build SQL query
+// Determine SQL query
 if (!empty($country)) {
-    // Partial search allowed
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 } else {
-    // No country given → return all
     $stmt = $conn->query("SELECT * FROM countries");
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table border="1">
+    <thead>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?= $row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
